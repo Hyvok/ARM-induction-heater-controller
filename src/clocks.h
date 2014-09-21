1cfd34f0dad2625acf34e033e154bfc3eb6f62a1
Kalle Hyvönen <kalle.hyvonen@dgkelectronics.com>
2014-09-21 12:39:18 +0300

    Commented clocks.h

--- src/clocks.h
+++ src/clocks.h
@@ -4,8 +4,23 @@
 #include "stm32f30x_new.h"
 #include "enable_disable_enum.h"
 
+/*--------------------------------------------------------------------*/
+// Clock system related functions 
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+// Enable clock for peripheral in AHB bus
+//
 void enableAhbPeriphClk(RCC_AHBENR_t periph, EN_t state);
+
+/*--------------------------------------------------------------------*/
+// Enable clock for peripheral in APB1 bus
+//
 void enableApb1PeriphClk(RCC_APB1ENR_t periph, EN_t state);
+
+/*--------------------------------------------------------------------*/
+// Enable clock for peripheral in APB2 bus
+//
 void enableApb2PeriphClk(RCC_APB2ENR_t periph, EN_t state);
 
 #endif // Include guard
