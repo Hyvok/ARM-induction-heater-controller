5150726fdd94157cdf9d85e85d43c18207c4f8e1
Kalle Hyvönen <kalle.hyvonen@dgkelectronics.com>
2014-09-21 12:49:31 +0300

    Commented pwm.h

--- src/pwm.h
+++ src/pwm.h
@@ -4,7 +4,18 @@
 #include <stdint.h>
 #include "stm32f30x_new.h"
 
+/*--------------------------------------------------------------------*/
+// PWM related functions
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+// Set PWM frequency
+//
 void setFreq(AC_TIM_t* tim, uint16_t val);
+
+/*--------------------------------------------------------------------*/
+// Set PWM pulse-width
+//
 void setPulseWidth(AC_TIM_t* tim, uint16_t val);
 
 #endif // Include guard
