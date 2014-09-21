2749fe3379a8f17d4f4cdd066296ad60807e9085
Kalle Hyvönen <kalle.hyvonen@dgkelectronics.com>
2014-09-21 12:46:11 +0300

    Commented macros.h

--- src/macros.h
+++ src/macros.h
@@ -1,8 +1,13 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+/*--------------------------------------------------------------------*/
 // Helper macros
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
 // Set register bits specified by mask to value
+//
 #define SET_MASK(reg, mask, val)    (reg = ((reg & ~mask)) | val)
 
 #endif // Include guard
